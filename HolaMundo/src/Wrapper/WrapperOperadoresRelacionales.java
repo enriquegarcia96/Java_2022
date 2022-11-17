2b5011a06088dfe92c13ac5478e4ef10ce25a47d
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-11-16 22:30:27 -0600

    refactor: 💡 Wrapper numericas y operadores relacionales

---
+++ HolaMundo/src/Wrapper/WrapperOperadoresRelacionales.java
@@ -0,0 +1,39 @@
+package Wrapper;
+
+public class WrapperOperadoresRelacionales {
+    public static void main(String[] args) {
+
+        Integer num1 = Integer.valueOf(1000);
+        Integer num2 = num1;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+
+        System.out.println("Son el mismo Objeto?  "  +  (num1 == num2));
+
+        num2 = 1000;
+
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+
+        // compara la instancia y no el valor de la variable
+        System.out.println("Son el mismo Objeto?  "  +  (num1 == num2));
+
+
+        System.out.println("Tienen el mismo valor ?  "  + (num1.equals(num2)));
+        System.out.println("Tienen el mismo valor ?  "  + (num1.intValue() == num2.intValue()));
+        
+        num2 = 500;
+        boolean condicion = num1 > num2;
+        System.out.println("condicion = " + condicion);
+
+        boolean condicion2 = num1.intValue() < num2.intValue();
+        System.out.println("condicion = " + condicion2);
+
+
+
+    }
+}
