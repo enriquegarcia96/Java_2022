bb24cacb3a42047a8028e7171e839c7d9a22948e
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-12-13 22:44:30 -0600

    refactor: 💡 utilizando enum con switch y iterando enum foreach

---
+++ HolaMundo/src/POO/EjemploAutomovilEnum.java
@@ -0,0 +1,58 @@
+package POO;
+
+public class EjemploAutomovilEnum {
+    public static void main(String[] args) {
+
+        Automovil.setCapacidadEstanqueEstatico(45);
+        Automovil subaru = new Automovil("Honda Civic","Impresa");
+
+        subaru.setCilindrada(2.0);
+        subaru.setColor(Color.BLANCO);
+        subaru.setTipo(TipoAutomovil.HATCHBACK);
+
+        Automovil mazda = new Automovil("Mazda","BT-50",Color.ROJO,3.0);
+        mazda.setTipo(TipoAutomovil.PICKUP);
+        System.out.println("manzda.fabricante = " +  mazda.getFabricante());
+
+
+        TipoAutomovil tipo = subaru.getTipo();
+        System.out.println("tipo tipoSubaru: " + tipo.getNombre());
+        System.out.println("tipo desc. tipoSubaru:  " + tipo.getDescripcion());
+
+        tipo = mazda.getTipo();
+        switch (tipo){
+            case CONVERTIBLE ->
+                System.out.println("El automovil es deportivo y descapotable de dos puertas");
+
+            case COUPE ->
+                System.out.println("Es un automovil pequeño de dos puertas y tipicamente deportivo");
+
+            case FURGON ->
+                System.out.println("Es un automovil utilitario de transporte, de empresas");
+
+            case HATCHBACK ->
+                System.out.println("Es un automovil mediano compacto, aspecto deportivo");
+
+            case PICKUP ->
+                System.out.println("Es un atomovil de doble cabina o camioneta");
+
+            case  SEDAN ->
+                System.out.println("Es un automovil mediano");
+
+            case STATION_WAGON ->
+                System.out.println("Es un automvil mas grande, con maleta grande...");
+        }
+
+        TipoAutomovil[]  tipos = TipoAutomovil.values();
+        for (TipoAutomovil ta: tipos){
+            System.out.print(ta + " => " +  ta.name() + ", "+
+                    ta.getNombre() +  ", " +
+                    ta.getDescripcion() + ", " +
+                    ta.getNumeroPuerta());
+            System.out.println();
+        }
+
+
+
+    }
+}
--- HolaMundo/src/POO/TipoAutomovil.java
+++ HolaMundo/src/POO/TipoAutomovil.java
@@ -1,12 +1,12 @@
 package POO;
 public enum TipoAutomovil {
     SEDAN("Sedan", "Auto normal", 4),
-    STATION_WAGON("Station Wagon", "Auto grande", 4),
-    HATCHBACK("Hatchback", "Auto compacto", 4),
+    STATION_WAGON("Station Wagon", "Auto grande", 5),
+    HATCHBACK("Hatchback", "Auto compacto", 5),
     PICKUP("Pickup", "Camioneta", 4),
     COUPE("Coupe", "Auto pequeño", 2),
     CONVERTIBLE("Convertible", "Auto deportivo", 2),
-    FURGON("Furgon",  "Auto utilitario", 4);
+    FURGON("Furgon",  "Auto utilitario", 3);
 
     private final String nombre;
     private final int numeroPuerta;
