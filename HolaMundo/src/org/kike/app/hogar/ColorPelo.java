937e885522c0f9050fac15cf5d93daf13d89f17e
enriquegarcia96 <enriquegarcia259@yahoo.es>
2023-01-09 22:30:40 -0600

    refactor: 💡 POO paquetes. static, modificadores de acceso

---
+++ HolaMundo/src/org/kike/app/hogar/ColorPelo.java
@@ -0,0 +1,5 @@
+package org.kike.app.hogar;
+
+public enum ColorPelo {
+    NEGRO, CAFE, CASTANIO, RUBIO, COLORIN
+}
---
+++ HolaMundo/src/org/kike/app/hogar/EjemploHogar.java
@@ -0,0 +1,18 @@
+package org.kike.app.hogar;
+import org.kike.app.jardin.Perro;
+import static org.kike.app.hogar.Persona.saludar;
+
+public class EjemploHogar {
+
+    public static void main(String[] args) {
+
+        Persona p = new Persona();
+        Perro perro = new Perro();
+        String saludo = saludar();
+
+
+
+    }
+
+
+}
