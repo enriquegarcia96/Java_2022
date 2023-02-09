0016f3ac3ac96b6e3fe17e7958d009fb8fe69dcb
enriquegarcia96 <enriquegarcia259@yahoo.es>
2023-02-08 22:14:51 -0600

    refactor: 💡 clases anonimas y clases abstractas validador

--- poo_clases_abstractas/src/org/garcia/pooclasesabstractas/form/EjemploForm.java
+++ poo_clases_abstractas/src/org/garcia/pooclasesabstractas/form/EjemploForm.java
@@ -3,7 +3,6 @@ package org.garcia.pooclasesabstractas.form;
 import org.garcia.pooclasesabstractas.form.elementos.*;
 import org.garcia.pooclasesabstractas.form.elementos.select.Opcion;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,25 +18,33 @@ public class EjemploForm {
         TextareaForm experiencia = new TextareaForm("exp", 5, 9);
 
         SelectForm lenguaje = new SelectForm("lenguaje");
-        Opcion java = new Opcion("1", "Java");
-        lenguaje.addOpcion(java)
-                .addOpcion(new Opcion("2", "Python"))
+        lenguaje.addOpcion(new Opcion("1", "Java"))
+                .addOpcion(new Opcion("2", "Python").setSelected())
                 .addOpcion(new Opcion("3", "JavaScript"))
                 .addOpcion(new Opcion("4", "TypeScript"))
                 .addOpcion(new Opcion("5", "PHP"));
 
+        ElementoForm saludar = new ElementoForm("saludo"){
 
+            @Override
+            public String dibujarHtml() {
+                return "<input disabled name='"+ this.nombre+"' value=\""+ this.valor +"\"  />";
+            }
+
+        };
+
+        saludar.setValor("Hola que tal este campo esta desabilitado!");
         username.setValor("Kike.doe");
         password.setValor("12345");
         email.setValor("kike@gmail.com");
         edad.setValor("27");
         experiencia.setValor("... mas de 10 años de experiencia ...");
-        java.setSelected(true);
+        //java.setSelected(true);
 
         // crea la lista
-        List<ElementoForm> elementos = Arrays.asList(username, password, email, edad, experiencia, lenguaje);
+        List<ElementoForm> elementos = Arrays.asList(username, password, email, edad, experiencia, lenguaje, saludar);
 
-        //lamda
+        //lada
         elementos.forEach(e -> {
             System.out.println(e.dibujarHtml());
             System.out.println("<br/>");
