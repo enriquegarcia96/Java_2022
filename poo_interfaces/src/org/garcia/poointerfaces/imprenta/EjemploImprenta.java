8f19dbd9d77a0d558bd28579138663ec301c22b6
enriquegarcia96 <enriquegarcia259@yahoo.es>
2023-02-16 22:41:25 -0600

    refactor: 💡 metodos default y static en interfaces

--- poo_interfaces/src/org/garcia/poointerfaces/imprenta/EjemploImprenta.java
+++ poo_interfaces/src/org/garcia/poointerfaces/imprenta/EjemploImprenta.java
@@ -1,20 +1,20 @@
 package org.garcia.poointerfaces.imprenta;
 
 import org.garcia.poointerfaces.imprenta.modelo.*;
-import org.garcia.poointerfaces.imprenta.modelo.Hoja;
 import static org.garcia.poointerfaces.imprenta.modelo.Genero.*;
+import static org.garcia.poointerfaces.imprenta.modelo.Imprimible.*;
 
 
 public class EjemploImprenta {
     public static void main(String[] args) {
-        Curriculo cv = new Curriculo("Andrea caceres","Lic. Pedagogia", "Resume laboral...");
+        Curriculo cv = new Curriculo(new Persona("Andrea", "Caceres"),"Lic. Pedagogia", "Resume laboral...");
         cv.addExperiencia("Java")
                 .addExperiencia("Oracle DBA")
                 .addExperiencia("Spring Framework")
                 .addExperiencia("Desarrollador fullstack")
                 .addExperiencia("Angular");
 
-        Libro libro = new Libro("Erich Gamma", "patrones de diseño: Elem. Reusables POO",
+        Libro libro = new Libro(new Persona("Enrique", "Garcia"), "patrones de diseño: Elem. Reusables POO",
                 PROGRAMACION);
         libro.addPagina(new Pagina("Patron Singleton"))
                 .addPagina(new Pagina("Patron Observador"))
@@ -22,15 +22,26 @@ public class EjemploImprenta {
                 .addPagina(new Pagina("Composite"))
                 .addPagina(new Pagina("Patron Facade"));
 
-        Informe informe = new Informe( "Martin forler", "James", "Estudio sobre microservicios");
+        Informe informe = new Informe(
+                        new Persona("Martin", "Fowler"),
+                        new Persona("James", "Golis"),
+                "Estudio sobre microservicios");
 
         imprimir(cv); // como tiene el contrato de la interface puede imprimir
         imprimir(informe);
         imprimir(libro);
+
+        Imprimible objImp = new Imprimible(){
+            @Override
+            public String imprimir() {
+                return "Hola que tal, imprimiendo un objeto generico de una clase anonima";
+            }
+        };
+
+        imprimir(objImp);
+        System.out.println(TEXTO_DEFECTO);
     }
 
-    public static void imprimir(Imprimible imprimible){
-        System.out.println(imprimible.imprimir());
-    }
+
 
 }
--- poo_interfaces/src/org/garcia/poointerfaces/imprenta/modelo/Imprimible.java
+++ poo_interfaces/src/org/garcia/poointerfaces/imprenta/modelo/Imprimible.java
@@ -1,7 +1,16 @@
 package org.garcia.poointerfaces.imprenta.modelo;
 
-public interface Imprimible {
+import com.sun.nio.sctp.AbstractNotificationHandler;
 
-    public String imprimir();
+public interface Imprimible {
+    String TEXTO_DEFECTO ="Imprimiendo un valor por defecto";
+
+    default String imprimir(){
+        return  TEXTO_DEFECTO ;
+    }
+
+    static void imprimir(Imprimible imprimible){
+        System.out.println(imprimible.imprimir());
+    }
 
 }
